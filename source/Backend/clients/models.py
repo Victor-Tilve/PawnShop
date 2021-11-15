fda15e7128ba7e07317b1c53c7767da110f03a9f
Victor Tilve <ing.victortilve@gmail.com>
2021-11-14 19:14:03 -0500

    updates modelos con null=false

--- source/Backend/clients/models.py
+++ source/Backend/clients/models.py
@@ -3,15 +3,15 @@ from django.core.validators import RegexValidator
 
 # Create your models here.
 class Client(models.Model):
-    nombre = models.CharField(max_length=200)
-    apellido = models.CharField(max_length=200)
-    cedula = models.CharField(max_length=10, validators=[RegexValidator(r'^\d{1,10}$')])
-    telefono = models.CharField(max_length=10, validators=[RegexValidator(r'^\d{1,10}$')])
-    direccion = models.CharField(max_length=200)
-    direccion2 = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200,null=False)
+    apellido = models.CharField(max_length=200,null=False)
+    cedula = models.CharField(max_length=10, validators=[RegexValidator(r'^\d{1,10}$')],null=False)
+    telefono = models.CharField(max_length=10, validators=[RegexValidator(r'^\d{1,10}$')],null=False)
+    direccion = models.CharField(max_length=200,null=False)
+    direccion2 = models.CharField(max_length=200,null=False)
 
-    ref1 = models.CharField(max_length=200)
-    telefono_ref1 = models.CharField(max_length=10, validators=[RegexValidator(r'^\d{1,10}$')])
+    ref1 = models.CharField(max_length=200,null=False)
+    telefono_ref1 = models.CharField(max_length=10, validators=[RegexValidator(r'^\d{1,10}$')],null=False)
              
     def __str__(self):
         return f"{self.nombre} {self.apellido}"
