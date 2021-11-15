54f51415a6ea11906880540cd41ffdb417d6ac59
Victor Tilve <ing.victortilve@gmail.com>
2021-11-14 19:13:21 -0500

    actualización de la applicación user.

--- source/Backend/users/forms.py
+++ source/Backend/users/forms.py
@@ -1,11 +1,13 @@
-from django.contrib.auth import get_user_model
+from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
-    class Meta:
-        model = get_user_model()
-        fields = ('email', 'username',)
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields
+
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
-        model = get_user_model()
-        fields = ('email', 'username',)
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
