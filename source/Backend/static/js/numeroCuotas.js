caf2a5153fac7543546aa6aef8db9a3eb4eed41a
Victor Tilve <ing.victortilve@gmail.com>
2021-11-24 23:20:13 -0500

    Mejoras - trabajando sobre form loans

--- source/Backend/static/js/numeroCuotas.js
+++ source/Backend/static/js/numeroCuotas.js
@@ -3,19 +3,14 @@ function NumCuotas(e) {
     let tipo_cuota = parseInt(e.target.value);
 
     if (tipo_cuota == 1) {
-        console.log("Condicion 1");
         document.getElementById("id_num_cuotas").value = num_meses;
         }else if (tipo_cuota == 2) {
-            console.log("Condicion 2");
             document.getElementById("id_num_cuotas").value = 2 * num_meses;
         }else if (tipo_cuota == 3) {
-            console.log("Condicion 3");
             document.getElementById("id_num_cuotas").value = 4 * num_meses;
         }else if (tipo_cuota == 4) {
-            console.log("Condicion 4");
             document.getElementById("id_num_cuotas").value = 30 * num_meses;
         }else{
-            console.log("Else");
             document.getElementById("id_num_cuotas").value = 0;
     }
 }
