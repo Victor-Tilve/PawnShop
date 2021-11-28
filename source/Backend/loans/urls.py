40f8548635b0494e261e675ddc7d6ccab0596cde
Victor Tilve <ing.victortilve@gmail.com>
2021-11-28 18:35:39 -0500

    loans url names

--- source/Backend/loans/urls.py
+++ source/Backend/loans/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-path('', views.loan_home_view),
-path('crear/', views.loan_create_view),
-path('buscar/', views.loan_search_view),
+path('', views.loan_home_view,name='loans-home'),
+path('crear/', views.loan_create_view,name='loans-crear'),
+path('buscar/', views.loan_search_view,name='loans-buscar'),
 ]
