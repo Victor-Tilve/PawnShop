4b497b874b3360386905ec73d3badfac13804b92
Victor Tilve <ing.victortilve@gmail.com>
2021-12-05 11:29:39 -0500

    using pyweb

---
+++ source/Backend/pywebview.py
@@ -0,0 +1,5 @@
+import webview
+
+webview.create_window('Hello world', 'http://127.0.0.1:8000/users/login/',fullscreen=True)
+# webview.create_window('Hello world', 'https://pywebview.flowrl.com/')
+webview.start(gui='qt')
