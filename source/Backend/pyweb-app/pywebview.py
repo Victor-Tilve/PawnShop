f2672b135077012ce3bbe55cccc5866af56e6470
Victor Tilve <ing.victortilve@gmail.com>
2022-01-27 21:20:24 -0500

    Cambio de dirección web

--- source/Backend/pyweb-app/pywebview.py
+++ source/Backend/pyweb-app/pywebview.py
@@ -1,5 +1,5 @@
 import webview
 
-# webview.create_window('Hello world', 'http://127.0.0.1:8000/users/login/',fullscreen=True)
-webview.create_window('Hello world', 'https://pywebview.flowrl.com/',fullscreen=True)
+webview.create_window('LUZUMAR', 'http://127.0.0.1:8000/users/login/',fullscreen=True)
+# webview.create_window('Hello world', 'https://pywebview.flowrl.com/',fullscreen=True)
 webview.start(gui='qt')
