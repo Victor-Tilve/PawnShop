426db3c02cd9ad516d10976c5cd7c7501ff4863e
Victor Tilve <ing.victortilve@gmail.com>
2022-01-31 21:16:01 -0500

    modificacion en abonos - views

--- source/Backend/abonos/views.py
+++ source/Backend/abonos/views.py
@@ -14,10 +14,26 @@ def abono_create_view(request):
 
     if request.method == 'POST':
         # print(request.POST)
+        allpost = request.POST
         form = AbonoForm(request.POST)
         if form.is_valid():
             # print('Is valid')
-            #TODO: Validate if the ID match to the client
+            
+            #----------
+            id_prestamo = allpost.get("prestamo", None)
+            # print(f"El id del prestamo: {id_prestamo}")
+            abono = allpost.get("abono", None)
+            # print(f"El abono es: {abono}")
+            # adeudado = Loan.objects.filter(pk=id_prestamo)
+            adeudado = list(Loan.objects.filter(pk=id_prestamo).values())[0]["monto_adeudado"]
+            print(f"Valor de deuda: {adeudado}")
+            print(f"tipo de dato: {type(adeudado)}")
+            new_adeudado = int(adeudado) - int(abono)
+            Loan.objects.filter(pk=id_prestamo).update(monto_adeudado=new_adeudado)
+            # _adeudado = list(Loan.objects.filter(pk=id_prestamo).values())[0]["monto_adeudado"]
+            # print(f"Valor del deuda a actualizar: {_adeudado}")
+        
+            #----------
             form.save()
             return redirect('/abonos/')  # 4
         else:  # 5
