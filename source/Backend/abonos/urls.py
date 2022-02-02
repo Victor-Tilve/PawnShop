a2282b8d011245c9f335f35d0740723c30a767f4
Victor Tilve <ing.victortilve@gmail.com>
2022-02-01 20:40:50 -0500

    quité un url repetida

--- source/Backend/abonos/urls.py
+++ source/Backend/abonos/urls.py
@@ -4,7 +4,6 @@ urlpatterns = [
 path('', views.abono_home_view,name='abonos-home'),
 path('crear/', views.abono_create_view,name='abonos-crear'),
 path('buscar/', views.abono_search_view,name='abonos-buscar'),
-path('buscar/', views.abono_search_view,name='abonos-buscar'),
 path('<int:pk>/', views.AbonosDetailView.as_view(), name='abono_detail'),
 path('get/ajax/validate/prestamos_cliente', views.check_prestamos_cliente, name = "check_prestamos_cliente"),
 path('get/ajax/validate/prestamo_informacion', views.check_prestamo_informacion, name = "check_prestamo_informacion"),
