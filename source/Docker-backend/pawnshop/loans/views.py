62e061202e96507038497c900ae27fc1d19d499b
Victor-Tilve <ing.victortilve@gmail.com>
2022-03-20 07:39:56 +0100

    se adelanta trabajo con vencidos y cobrarr hoy

--- source/Docker-backend/pawnshop/loans/views.py
+++ source/Docker-backend/pawnshop/loans/views.py
@@ -28,7 +28,6 @@ def loan_create_view(request):
         }
     return render(request, 'loans/prestamos_crear.html', context)
 
-
 def loan_home_view(request):
     clientes = Client.objects.all().order_by('pk')
     prestamos = Loan.objects.all().order_by('pk')
@@ -43,8 +42,31 @@ def loan_home_view(request):
 def loan_search_view(request):
     return render(request, 'loans/prestamos_buscar.html', {})
 
-
 def tabla_prestamo(request):
+    if request.is_ajax and request.method == "GET":
+        tables = []
+        loans_table = Loan.objects.values()
+        for prestamo in loans_table:
+            # cliente = list(Client.objects.filter(pk=prestamo.cliente))
+            cliente_nombre = list(Client.objects.filter(pk=prestamo["cliente_id"]).values())[0]["nombre"]
+            cliente_apellido = list(Client.objects.filter(pk=prestamo["cliente_id"]).values())[0]["apellido"]
+            tables.append(
+                f'<tr><th scope="row">{prestamo["id"]}</th><td>{prestamo["monto_prestado"]}</td><td>{prestamo["monto_adeudado"]}</td><td>{prestamo["num_meses"]}</td><td>{prestamo["cliente_id"]}</td><td>{cliente_nombre} {cliente_apellido}</td></tr>'
+            )
+        
+        return JsonResponse({"_prestamo_imformacion":tables}, status = 200)
+
+        
+    return JsonResponse({}, status = 400)
+
+def loan_cobrar_hoy(request):
+    
+    return render(request, 'loans/prestamos_cobrar_hoy.html')
+
+def loan_vencidos(request):
+    return render(request, 'loans/prestamos_vencidos.html')
+
+def tabla_vencidos(request):
     if request.is_ajax and request.method == "GET":
         tables = []
         loans_table = Loan.objects.values()
